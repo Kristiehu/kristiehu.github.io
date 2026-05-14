d1986b55a4c445dc04ed6a8693eb4cd2fc8bdbda
Kristie Hu <kristiehu@Kristies-MacBook-Pro.local>
2026-05-13 23:01:34 -0400

    update website icon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Kristie Hu · Spatial Systems & ML";
+export const SITE_TITLE = "Kristie Hu · Spatial Systems & ML 🛰️";
 export const SITE_DESCRIPTION = "Geospatial ML, indoor LiDAR, and spatial data systems. PhD researcher · Toronto.";
