f2c7794fa43b879822bfd8c53868fe81ddd95efa
Kristiehu <kristieyet@gmail.com>
2026-06-27 15:58:13 -0400

    consts.ts update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Kristie Hu · Spatial Systems & ML";
-export const SITE_DESCRIPTION = "Geospatial ML, indoor LiDAR, and spatial data systems. PhD researcher · Toronto.";
+export const SITE_TITLE = "Kristie J. Hu | LiDAR & Remote Sensing";
+export const SITE_DESCRIPTION =
+  "Official website of Kristie Jingyi Hu, GIS Application Analyst and geospatial researcher specializing in LiDAR, remote sensing, computer vision, spatial ETL, and digital twins.";
