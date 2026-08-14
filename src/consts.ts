65c06e7b3040405c5f47a5a562656540d5f1db6f
Kristie <44983605+Kristiehu@users.noreply.github.com>
2026-08-14 13:36:26 -0400

    update the header format

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Kristie J. Hu | LiDAR & Remote Sensing";
+export const SITE_TITLE = "Kristie J. Hu";
 export const SITE_DESCRIPTION =
   "Official website of Kristie Jingyi Hu, GIS Application Analyst and geospatial researcher specializing in LiDAR, remote sensing, computer vision, spatial ETL, and digital twins.";
